59beb91f912797664e814242edad448c4cd4b0a9
annayqho <annayqho@gmail.com>
2017-11-29 00:41:16 -0800

    redo

--- display.php
+++ display.php
@@ -1,8 +1,36 @@
+<!doctype html>
 <html>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Anna Ho by annayqho</title>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/github-light.css">
+    <meta name="viewport" content="width=device-width">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
 
-</body>
+      <h1><a href="https://annayqho.github.io/#ztf">ZTF</a></h1>
+        <a href="#sched">ZTF Meeting Schedule</a> <br>
+        <a href="#cal">ZTF Superbias Frames</a> <br>
+        <a href="#individual">Visualize individual frames</a> <br>
+
+      </header>
+
+      <section>
+    
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your email address is: <?php echo $_POST["email"]; ?>
+
+      </section>
+
+    </div>
+
+  </body>
 </html>
